6f537efa870abbcc0803dcf8fc4f1db0adf490ca
Ana Alice Honorio <ana.alice.honorio@gmail.com>
2024-10-05 17:47:36 -0300

    loading estilizado

--- frontend/src/components/Loading/Loading.js
+++ frontend/src/components/Loading/Loading.js
@@ -1,6 +1,12 @@
 import React from "react";
+import "./Loading.css";
+
 const Loading = () => {
-  return <div>Carregando...</div>;
+  return (
+    <div className="loading-container">
+      <div className="loading-message">Carregando...</div>
+    </div>
+  );
 };
 
 export default Loading;
